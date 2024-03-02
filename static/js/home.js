f638a7d9e2a1c07b82dd4a396a981c7768fb0057
Harish <harishsathya03@gmail.com>
2024-03-02 17:33:51 +0530

    big stuff uff

---
+++ static/js/home.js
@@ -0,0 +1,9 @@
+// Example: Add a simple hover effect
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.1)';
+    });
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
